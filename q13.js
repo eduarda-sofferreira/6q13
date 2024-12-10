233cb210445cd47497d3bbb6afad1ebfd81718ef
eduarda-sofferreira <espf1@aluno.ifal.edu.br>
2024-12-09 21:49:09 -0300

    Create q13.js

---
+++ q13.js
@@ -0,0 +1,11 @@
+var c = 1
+var ate100 = 0
+while ( c <= 20) {
+    var numero = parseInt (prompt ("informe um número"))
+    if ( numero >= 0 && <= 100 0){
+        ate100++;
+    }
+    c++
+}
+
+    console.log(ate100 + "números estão entre 0 e 100")
